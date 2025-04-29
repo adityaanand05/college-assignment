5a075f4dd4522d7b59f1a34a16d1ddbf27f202b8
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-04-29 23:11:42 +0530

    modified:   code 6/Q6.c 	new file:   code 6/Q6.exe 	new file:   code 6/Q7.c

--- code 6/Q6.c
+++ code 6/Q6.c
@@ -2,7 +2,20 @@
 
 #include <stdio.h>
 
-int main() {
-    
+int main()
+{
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    if (a == 0)
+    {
+        printf("%d is nither Odd nor Even", a);
+        
+    }else if (a%2 == 0){
+        printf("%d is Even", a);
+    }else{
+        printf("%d is Odd", a);
+    }
+
     return 0;
 }
