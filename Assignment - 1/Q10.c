8ed6bb44337c12fd8e7448db5509f6dd7510158e
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-04-30 00:37:26 +0530

    modified:   Assignment - 1/Q10.c 	new file:   Assignment - 1/Q10.exe

--- Assignment - 1/Q10.c
+++ Assignment - 1/Q10.c
@@ -2,10 +2,16 @@
 // taken from the keyboard. As for example if the character taken from the keyboard is ‘B’ the program will
 // print the immediate next character ‘C’. Also, print the corresponding ASCII value.
 
-#include <stdio.h>
+#include<stdio.h>
+ 
+int main(){
+   char a;
 
-int main() {
-    printf("Hello Aditya");
-    
-    return 0;
+   printf("enter a  char value  :");
+   scanf("%c", &a);
+
+   printf("the next ASCII character is %c\n", a+1);
+   printf("the corresponding ASCII value  is %d", a+1);
+
+   return 0;
 }
